a2bfa7e6723060074519eccad5d54b8eb2cc9aa4
d1ko <58879685+d1ko@users.noreply.github.com>
2023-09-03 00:37:25 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+const PrototypeForm = {
+  key1: 'Barbara',
+  key2: 'Hepworth',
+  key3: 'bhepworth@react.com',
+  key4: 'love cats',
+};
+
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="form-container">
+      <form>
+        <div className="form-group">
+          <label htmlFor="key1">First Name</label>
+          <input type="text" id="key1" value={PrototypeForm.key1} readOnly />
+          <p className="form-data">{PrototypeForm.key1}</p>
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="key2">Last Name</label>
+          <input type="text" id="key2" value={PrototypeForm.key2} readOnly />
+          <p className="form-data">{PrototypeForm.key2}</p>
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="key3">Email</label>
+          <input type="email" id="key3" value={PrototypeForm.key3} readOnly />
+          <p className="form-data">{PrototypeForm.key3}</p>
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="key4">Hobbies</label>
+          <input type="text" id="key4" value={PrototypeForm.key4} readOnly />
+          <p className="form-data">{PrototypeForm.key4}</p>
+        </div>
+      </form>
     </div>
   );
-}
+};
 
-export default App;
